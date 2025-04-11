40e690e6429172f8a9c1ef01abcd6c3946dc678b
svliy <878184941@qq.com>
2025-04-11 16:28:12 +0800

    修改运行脚本run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,13 @@
-CUDA_VISIBLE_DEVICES=3 python train.py --config_name bp4d2gft
-CUDA_VISIBLE_DEVICES=3 python train_stage2.py --config_name bp4d2gft
+# bp4d ---> gft
+# source
 
-CUDA_VISIBLE_DEVICES=0 python train_for_test.py --config_name bp4d2gft
-
-CUDA_VISIBLE_DEVICES=2 python train_target.py --config_name bp4d2disfa
+# cross: stage 1
+CUDA_VISIBLE_DEVICES=1 python train.py --config_name bp4d2gft
+# cross: stage 2
+CUDA_VISIBLE_DEVICES=1 python train_stage2.py --config_name bp4d2gft
+# target
 CUDA_VISIBLE_DEVICES=1 python train_target.py --config_name bp4d2gft
 
-CUDA_VISIBLE_DEVICES=1 python train_target.py --config_name bp4d
-
-
+# run back
 CUDA_VISIBLE_DEVICES=1 nohup python -u train_target.py --config_name bp4d2gft > /dev/null 2>&1 &
 CUDA_VISIBLE_DEVICES=2 nohup python -u train_target.py --config_name bp4d2disfa > /dev/null 2>&1 &
-
-python train_target.py --config_name bp4d2gft
